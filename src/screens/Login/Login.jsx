8fe550e23882a5a5a924110b9377d3083a0adb0f
alyaamobarak <ama789238@gmail.com>
2025-05-03 00:27:18 +0300

    add home content

--- src/screens/Login/Login.jsx
+++ src/screens/Login/Login.jsx
@@ -4,7 +4,7 @@ const Login = () => {
 
   return (
     <div className="login-container">
-      <div className="login-box">
+      <div className="log-box">
         <div className="mb-3">
           <img src="/imgs07.png" alt="logo" width="65px" />
         </div>
