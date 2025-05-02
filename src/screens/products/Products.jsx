60a371be9387c42206068894312ce303424d20de
ahmedali <ahmedalbaseony14@gmail.com>
2025-05-02 19:58:03 +0300

    test url of products in console

--- src/screens/products/Products.jsx
+++ src/screens/products/Products.jsx
@@ -1,13 +1,25 @@
 
-
+import axios from 'axios';
 import './Products.css'
+import { useEffect, useState } from 'react';
 
 const Products = () => {
 
+  const [products, setproducts] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/v1/products")
+      .then((res) => setproducts(res.data) )
+      .catch((err) => console.error("Error get products:", err));
+      
+  }, []);
+  
   return (
     <>
-
+    
+    {console.log(products.data?.[0].images[0])}
+    
     </>
   );
 };
