d2e427e6ed1a9ebb480ee4ddbd6e1db3654ee60b
alyaamobarak <ama789238@gmail.com>
2025-05-09 16:58:44 +0300

    add

--- src/screens/Products/Product.jsx
+++ src/screens/Products/Product.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import styles from './Product.module.css';
 import { Link } from "react-router-dom";
 
@@ -58,3 +58,71 @@ const Products = () => {
 
 
 export default Products;
+
+// import axios from "axios";
+// import React, { useEffect, useState } from "react";
+// import styles from './Product.module.css';
+// import { Link } from "react-router-dom";
+
+// const Products = () => {
+//   const [productsList, setProductList] = useState([]);
+
+//   useEffect(() => {
+//     axios.get("http://localhost:3000/api/v1/products")
+//       .then(res => {
+//         console.log("productsList", res.data.data); 
+//         setProductList(res.data.data);
+//       })
+//       .catch(err => console.error(err));
+//   }, []);
+
+//   return (
+//     <div className="container my-5">
+
+//       <h2 className={`text-end fs-4 fw-bold my-4 p-2 ${styles.bgBauty}`}>اخر العروض</h2>
+//       <div className="row">
+//         {productsList.slice(0, 6).map((product) => (
+//           <div key={product._id} className="col-md-2">
+//             <Link to={`/products/${product._id}`} className="text-decoration-none text-dark">
+//               <div className={styles.productCard}>
+//                 {product.images && product.images.length > 0 && product.images[0].secure_url ? (
+//                   <img className="w-100 img-fluid" src={product.images[0].secure_url} alt={product.name} />
+//                 ) : (
+//                   <img className="w-100 img-fluid" src="/placeholder.jpg" alt="No Image" />
+//                 )}
+//                 <div className="text-end p-2">
+//                   <h6 className="m-0">{product.name}</h6>
+//                   <span>{product.price} جنيه</span>
+//                 </div>
+//               </div>
+//             </Link>
+//           </div>
+//         ))}
+//       </div>
+
+//       <h2 className={`text-end fs-4 fw-bold my-4 p-2 ${styles.bgFff2d6}`}>منتجات مختارة</h2>
+//       <div className="row">
+//         {productsList.slice(6, 11).map((product) => (
+//           <div key={product._id} className="col-md-2">
+//             <Link to={`/products/${product._id}`} className="text-decoration-none text-dark">
+//               <div className={styles.productCard}>
+//                 {product.images && product.images.length > 0 && product.images[0].secure_url ? (
+//                   <img className="w-100 img-fluid" src={product.images[0].secure_url} alt={product.name} />
+//                 ) : (
+//                   <img className="w-100 img-fluid" src="/placeholder.jpg" alt="No Image" />
+//                 )}
+//                 <div className="text-end p-2">
+//                   <h6 className="m-0">{product.name}</h6>
+//                   <span>{product.price} جنيه</span>
+//                 </div>
+//               </div>
+//             </Link>
+//           </div>
+//         ))}
+//       </div>
+
+//     </div>
+//   );
+// };
+
+// export default Products;
